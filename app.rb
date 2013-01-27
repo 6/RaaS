0fc6acf1ed79a391de9f4915ee66bf690b4c9ce6
Peter Graham <peterghm@gmail.com>
2013-01-26 21:46:07 -0800

    Create a module for shared code

--- app.rb
+++ app.rb
@@ -6,34 +6,36 @@ require 'rest-client'
 require 'sinatra'
 
 class App < Sinatra::Base
-  def go
+  get '/:method' do
+    RequestHandler.go(self, params)
+  end
+
+  post '/:method' do
+    RequestHandler.go(self, params)
+  end
+end
+
+module RequestHandler
+  def self.go(context, params)
     method = params[:method].andand.to_sym || :get
     forced_encoding = params[:force].andand.strip
     unless [:get, :post, :put, :delete, :head, :patch].include?(method)
-      return Response.send(self, error: "Unsupported method: #{params[:method]}")
+      return Response.send(context, error: "Unsupported method: #{params[:method]}")
     end
     if params[:url].nil? || params[:url].strip == ""
-      return Response.send(self, error: "No URL specified")
+      return Response.send(context, error: "No URL specified")
     end
     url = Addressable::URI.parse(params[:url].strip).normalize.to_str
     begin
       response = Request.send(url: url, method: method)
-      return Response.send(self, response: response, force: forced_encoding)
+      return Response.send(context, response: response, force: forced_encoding)
     rescue => e
       if e.is_a?(RestClient::Exception)
-        return Response.send(self, response: e.response)
+        return Response.send(context, response: e.response)
       else
-        return Response.send(self, error: e.class.name)
+        return Response.send(context, error: e.class.name)
       end
     end
-  end
-
-  get '/:method' do
-    go
-  end
-
-  post '/:method' do
-    go
   end
 end
 
