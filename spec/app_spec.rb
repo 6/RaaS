c2d42b24a70e01b5f147dcb952d1a3cc44b08aac
Peter Graham <peterghm@gmail.com>
2013-01-27 14:28:11 -0800

    Test invalid URL

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -54,4 +54,21 @@ describe 'app' do
       response_json['error'].should include("No URL specified")
     end
   end
+
+  context "with an invalid URL" do
+    before do
+      stub_request(:get, "http://invalid").to_raise(SocketError)
+      post "/get", {url: "http://invalid"}
+    end
+
+    it "responds with 400" do
+      last_response.status.should == 400
+    end
+
+    it "includes SocketError in the error message" do
+      response_json = JSON.parse(last_response.body)
+
+      response_json['error'].should include("SocketError")
+    end
+  end
 end
