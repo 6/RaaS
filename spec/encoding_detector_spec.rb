9debd573e3a2b9a7585afb0d421660b86bcdfd9b
Peter Graham <peterghm@gmail.com>
2013-01-27 16:31:27 -0800

    Test EncodingDetector

---
+++ spec/encoding_detector_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe EncodingDetector do
+  context "when it returns confidence lower than the cutoff" do
+    it "returns the default encoding" do
+      CharlockHolmes::EncodingDetector.should_receive(:detect).and_return(confidence: 5, encoding: "GB2312")
+
+      encoding = EncodingDetector.detect(string: "a string", default: "Shift_JIS")
+      encoding.should == "Shift_JIS"
+    end
+  end
+
+  context "when it returns a confidence greater than or equal to the cutoff" do
+    it "returns the detected encoding" do
+      CharlockHolmes::EncodingDetector.should_receive(:detect).and_return(confidence: 60, encoding: "GB2312")
+
+      encoding = EncodingDetector.detect(string: "a string", default: "Shift_JIS")
+      encoding.should == "GB2312"
+    end
+  end
+
+  context "when an execption is raised while attempting to detecting encoding" do
+    it "returns the default encoding" do
+      CharlockHolmes::EncodingDetector.should_receive(:detect).and_raise(StandardError)
+
+      encoding = EncodingDetector.detect(string: "a string", default: "Shift_JIS")
+      encoding.should == "Shift_JIS"
+    end
+  end
+end
